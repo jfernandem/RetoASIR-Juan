88c28e942d3d97222458bca6881d91e60eeecda3
jfernandem <156295097+jfernandem@users.noreply.github.com>
2024-01-25 13:49:30 +0100

    Update EC2-CargaDatos.sh

--- EC2-CargaDatos.sh
+++ EC2-CargaDatos.sh
@@ -20,3 +20,14 @@ apt install postgresql postgresql-contrib -y
 #coturn
 apt update -y
 apt install coturn -y
+
+#Docker
+sudo apt update
+sudo apt upgrade -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt update
+sudo apt install docker-ce -y
+
+sudo systemctl status docker
